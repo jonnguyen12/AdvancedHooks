21ec9729b631b26d0d4b554e548347c06f9100e6
Willie Yam <willieyam7@gmail.com>
2021-06-22 14:35:30 -0400

    section 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,113 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import PurchaseButton from "../components/buttons/PurchaseButton"
+import CourseCard from "../components/cards/CourseCard"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+  <Wrapper>
+    <HeroWrapper>
+      <CourseCard />
+      <TextWrapper>
+        <Logo src="/images/logos/react-logo.svg" alt="logo" />
+        <Title>Build a web app with React Hooks</Title>
+        <Caption>20 sections - 3 hours of videos</Caption>
+        <Description>
+          Learn how we build the new DesignCode website with React Hooks
+        </Description>
+        <AuthorWrapper>
+          <AuthorImage src="/images/avatars/Meng.png" alt="avatar" />
+          <Caption>Taught by Meng To</Caption>
+        </AuthorWrapper>
+        <PurchaseButton />
+        <SmallText>
+          Purchase includes access to 30 courses. Over 80 hours of content,
+          including 12 hours for SwiftUI, for iOS 13 and iOS 14.
+        </SmallText>
+      </TextWrapper>
+    </HeroWrapper>
+  </Wrapper>
 )
 
 export default IndexPage
+
+const Wrapper = styled.div`
+  background: linear-gradient(200.44deg, #4316db 13.57%, #9076e7 98.38%);
+`
+
+const TextWrapper = styled.div`
+  position: relative;
+  display: grid;
+  gap: 20px;
+
+  @media (max-width: 780px) {
+    justify-items: center;
+    text-align: center;
+  }
+`
+
+const HeroWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 360px auto;
+  max-width: 1234px;
+  gap: 60px;
+  padding: 220px 20px 0px;
+  justify-content: center;
+  margin: 0 auto;
+
+  @media (max-width: 780px) {
+    grid-template-columns: 1fr;
+    justify-items: center;
+  }
+`
+
+const AuthorWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`
+
+const AuthorImage = styled.img`
+  width: 32px;
+  height: 32px;
+`
+
+const Logo = styled.img`
+  width: 60px;
+  height: 60px;
+`
+
+const Title = styled.h1`
+  max-width: 500px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 60px;
+  line-height: 72px;
+  color: #ffffff;
+  mix-blend-mode: normal;
+  text-shadow: 0px 20px 40px rgba(0, 0, 0, 0.3);
+`
+
+const Caption = styled.p`
+  font-style: normal;
+  font-size: 15px;
+  line-height: 130%;
+  text-transform: uppercase;
+  color: rgba(255, 255, 255, 0.7);
+`
+
+const Description = styled.p`
+  max-width: 400px;
+  font-style: normal;
+  font-size: 20px;
+  line-height: 140%;
+  color: #ffffff;
+`
+
+const SmallText = styled.p`
+  max-width: 280px;
+  font-style: normal;
+  font-size: 13px;
+  line-height: 130%;
+  color: rgba(255, 255, 255, 0.7);
+`
